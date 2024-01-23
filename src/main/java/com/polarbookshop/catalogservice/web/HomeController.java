56f8b88456c57068393f07805d63755fd62e3938
joseps <mel17800>
2024-01-23 11:53:09 -0600

    Integration tests, SpringDataJDBC, DB Auditing, Test Containers, Flyway

--- src/main/java/com/polarbookshop/catalogservice/web/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/web/HomeController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @Value("${JAVA_HOME}")
-    private String javaHome;
-
     private final PolarProperties polarProperties;
 
     public HomeController(PolarProperties polarProperties) {
@@ -21,11 +18,5 @@ public class HomeController {
     public String getGreeting() {
         return polarProperties.getGreeting();
     }
-
-    @GetMapping("/env")
-    public String getEnvironment() {
-        return javaHome;
-    }
-
 
 }
